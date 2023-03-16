b39dbd03083407a896b25c498988f7f2e5a79f83
Angelica <lisoveckaya1997@gmail.com>
2023-03-16 14:30:50 +0200

    task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('[data-start]');
+console.log(startBtn);
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = 'true';
+  stopBtn.removeAttribute('disabled');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.disabled = 'true';
+});
