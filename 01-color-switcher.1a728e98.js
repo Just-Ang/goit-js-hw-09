41706fe60e67c7e9a2b903b20fa0874975a545f2
Just-Ang <lisoveckaya1997@gmail.com>
2023-03-19 12:24:57 +0000

    Deploying to gh-pages from @ Just-Ang/goit-js-hw-09@7c0c709a7a16761385156c99dd0de17fa01086d2 🚀

--- 01-color-switcher.1a728e98.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-start]");console.log(e);var t=document.querySelector("[data-stop]"),r=document.querySelector("body");e.addEventListener("click",(function(){setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3);e.disabled="true",t.removeAttribute("disabled")})),t.addEventListener("click",(function(){clearInterval(timerId),e.removeAttribute("disabled"),t.disabled="true"}))}();
-//# sourceMappingURL=01-color-switcher.1a728e98.js.map
